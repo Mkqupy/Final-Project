86d750b92a690714c713389ffd3e7a17a43adb81
Mkqupy <121451774+Mkqupy@users.noreply.github.com>
2024-07-26 19:00:35 +0300

    save progress

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -75,3 +75,24 @@
 
 // const postsAPI = new PostsAPI();
 // export default postsAPI;
+
+
+let event1 = document.querySelector('.event1');
+let modal1 = document.querySelector('.modal1');
+let closeBtn = document.querySelector('.closeBtn');
+let backdrop = document.querySelector('.backdrop');
+
+event1.addEventListener( 'click', () => {
+    modal1.classList.add('active1');
+    backdrop.classList.add('activeBackdrop');
+});
+
+closeBtn.addEventListener( 'click', () => {
+    modal1.classList.remove('active1');
+    backdrop.classList.remove('activeBackdrop');
+});
+
+backdrop.addEventListener( 'click', () => {
+    modal1.classList.remove('active1');
+    backdrop.classList.remove('activeBackdrop');
+});
